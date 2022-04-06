85b3bcac55ce0f6726d13dba0da8c969df5ef709
lautarobarrera10 <lautarobarrera12@gmail.com>
2022-04-06 08:34:39 -0300

    splash screen creada

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// --- SPLASH SCREEN ---
+const splash = document.querySelector('.splash');
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    setTimeout(()=>{
+        splash.classList.add('display-none');
+    }, 2000)
+})
